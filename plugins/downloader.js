43586d848c387864bd677ed1c4d91071fdd4dc1b
mrsky <126326692+mrsky-cyber@users.noreply.github.com>
2024-06-30 12:09:41 +0300

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -75,7 +75,7 @@ const {
 *Followers:* ${followers}
 *Following:* ${following}
 
-\t*VENOCYBER IG STALKER*
+\t*MRSKY IG STALKER*
 `;
 
       await m.bot.sendFromUrl(m.from, photo_profile, caption, m, {}, "image");
@@ -319,7 +319,7 @@ smd(
    {
      pattern: "allsocial",
      desc: "Download media from various social platforms.",
-     category: "downloader",
+     category: "Facebook",
      filename: __filename,
      use: "<url>",
    },
@@ -430,11 +430,11 @@ smd(
          url: songUrl,
        } = data.result;
  
-       let output = `*Song:* ${song}\n`;
-       output += `*Artist:* ${artist.join(", ")}\n`;
-       output += `*Album:* ${album_name}\n`;
-       output += `*Release Date:* ${release_date}\n\n`;
-       output += `*Cover Image:* ${cover_url}\n\n`;
+       let output = `*🌞Song:* ${song}\n`;
+       output += `*🌞Artist:* ${artist.join(", ")}\n`;
+       output += `*🌞Album:* ${album_name}\n`;
+       output += `*🌞Release Date:* ${release_date}\n\n`;
+       output += `*🌞Cover Image:* ${cover_url}\n\n`;
  
        const buffer = await axios.get(songUrl, { responseType: "arraybuffer" });
        const fileName = `${song.replace(/\s/g, "_")}.mp3`;
@@ -630,7 +630,7 @@ smd(
          let _0x13ee38 = await getBuffer(_0x3c2608);
          await _0x19df48.reply(_0x13ee38, {
            packname: Config.packname,
-           author: "Venocyber-Md"
+           author: "Mrsky-Md"
          }, "sticker");
        } else {
          await _0x19df48.bot.sendMessage(_0x19df48.chat, {
@@ -649,7 +649,7 @@ smd(
    {
      pattern: "fbaudio",
      desc: "Downloads Facebook videos in audio.",
-     category: "downloader",
+     category: "Facebook",
      filename: __filename,
      use: "<add Facebook URL>",
    },
@@ -721,7 +721,7 @@ smd(
      }
  
      const { thumbnail, url: mediaUrl, wm } = mediaData;
-     const caption = `*Watermark:* ${wm}\n\n_Note: This media may have a watermark._`;
+     const caption = `*Mrsky Video Cover:* ${wm}\n\n_Note: This media may have a watermark._`;
  
      await m.bot.sendFromUrl(m.from, thumbnail, caption, m, {}, "image");
      await m.bot.sendFromUrl(m.from, mediaUrl, "", m, {}, "video");
@@ -902,7 +902,7 @@ smd(
      pattern: "facebook",
      alias: ["fb", "fbdl"],
      desc: "Downloads Facebook videos.",
-     category: "downloader",
+     category: "Facebook",
      filename: __filename,
      use: "<add Facebook URL>",
    },
@@ -948,7 +948,7 @@ smd(
      pattern: "facebook2",
      alias: ["fbhd"],
      desc: "Downloads Facebook videos in HD.",
-     category: "downloader",
+     category: "Facebook",
      filename: __filename,
      use: "<add Facebook URL>",
    },
@@ -1010,10 +1010,10 @@ smd(
      }
      const _0x48bc12 = parseInt(_0x538b40.size);
      if (_0x48bc12 > 200) {
-       return _0x7b09ff.reply("❌ File size bigger than 200mb.");
+       return _0x7b09ff.reply("😢 File size bigger than 200mb.");
      }
      const _0x31321c = _0x538b40.dllink;
-     let _0x24f726 = await fancytext("『 *ᗩᑭᏦ  ᗞᝪᗯᑎしᝪᗩᗞᗴᖇ* 』\n\n*APP Name :* " + _0x538b40.name + "\n*App Id :* " + _0x538b40.package + "\n*Last Up :* " + _0x538b40.lastup + "\n*App Size :* " + _0x538b40.size + "\n\n\n " + Config.caption, 25);
+     let _0x24f726 = await fancytext("『 *ᗩᑭᏦ  ᗞᝪᗯᑎしᝪᗩᗞᗴᖇ* 』\n\n*📲APP Name :* " + _0x538b40.name + "\n*📲App Id :* " + _0x538b40.package + "\n*📲Last Up :* " + _0x538b40.lastup + "\n*📲App Size :* " + _0x538b40.size + "\n\n\n " + Config.caption, 25);
      const _0x3e266b = (_0x538b40?.name || "Downloader") + ".apk";
      const _0x585f79 = "temp/" + _0x3e266b;
      let _0x533c85 = await _0x7b09ff.reply(_0x538b40.icon, {
@@ -1066,7 +1066,7 @@ smd(
      const _0x4ac8f2 = await search(_0x1cb962);
      if (_0x4ac8f2.length) {
        let _0x3d85b = await download(_0x4ac8f2[0].id);
-       let _0x307e6f = "*VENOCYBER-ᴍᴅ • ᴀᴘᴋ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪsᴛ* \n*________________________________* \n\n*_Reply Any Number To Download._*\n_Results For : " + _0x1cb962 + "_ \n";
+       let _0x307e6f = "*MRSKY-ᴍᴅ • ᴀᴘᴋ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪsᴛ* \n*________________________________* \n\n*_Reply Any Number To Download._*\n_Results For : " + _0x1cb962 + "_ \n";
        for (let _0x5a5920 = 0; _0x5a5920 < _0x4ac8f2.length; _0x5a5920++) {
          _0x307e6f += "\n*" + (_0x5a5920 + 1) + " : " + _0x4ac8f2[_0x5a5920].name + "* \n*Id : " + _0x4ac8f2[_0x5a5920].id + "* \n";
        }
@@ -1152,7 +1152,7 @@ smd(
          : "";
        if (!_0x1c586e) {
          return await _0x1ae8f8.reply(
-           "*Provide Repo Url, _.gitclone https://github.com/kingjux/Venocyber-Md_*"
+           "*Provide Repo Url, _.gitclone https://github.com/mrsky-cyber/mrsky-Md_*"
          );
        }
        const _0x5906ab =
@@ -1202,7 +1202,7 @@ smd(
    try {
      let _0x204f81 = _0x55aba2.reply_text ? _0x55aba2.reply_text : _0x56da6b;
      if (!_0x204f81) {
-       return _0x55aba2.reply("*_Example : .tts Hi,I am Venocyber-Md whatsapp bot._*");
+       return _0x55aba2.reply("*_Example : .tts Hi,I am Mrsky-Md whatsapp bot._*");
      }
      try {
        let _0x1974d5 = _0x56da6b ? _0x56da6b.split(" ")[0].toLowerCase() : "en";
@@ -1590,7 +1590,7 @@ smd(
          return m.reply("*_Could not find the file!_*");
        }
  
-       const caption = `『 *VENOCYBER-ᴍᴅ Mᴇᴅɪᴀғɪʀᴇ Dᴏᴡɴʟᴏᴀᴅᴇʀ* 』\n\n *Name* : ${result.name}\n *Size* : ${result.size}\n *Mime* : ${result.mime}\n\n\n${Config.caption}`;
+       const caption = `『 *MRSKY-ᴍᴅ Mᴇᴅɪᴀғɪʀᴇ Dᴏᴡɴʟᴏᴀᴅᴇʀ* 』\n\n *Name* : ${result.name}\n *Size* : ${result.size}\n *Mime* : ${result.mime}\n\n\n${Config.caption}`;
        const fancyCaption = await fancytext(caption, 25);
        const contextInfo = {
          ...(await m.bot.contextInfo(Config.botname, "MEDIAFIRE")),
@@ -1626,7 +1626,7 @@ smd(
      }
      let _0x3b2ca6 = await yts(_0x4ec99f);
      let _0x4123ae = _0x3b2ca6.all[0];
-     let _0x5883a9 = "\t *VENOCYBER-ᴍᴅ • sᴏɴɢ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ*   \n\n*Title :* " + _0x4123ae.title + "\nUrl : " + _0x4123ae.url + "\n*Description :* " + _0x4123ae.timestamp + "\n*Views :* " + _0x4123ae.views + "\n*Uploaded :* " + _0x4123ae.ago + "\n*Author :* " + _0x4123ae.author.name + "\n\n\n_Reply 1 for Video_ Or _1 document_\n_Reply 2 for Audio_ Or _2 document_";
+     let _0x5883a9 = "\t *MRSKY-ᴍᴅ • sᴏɴɢ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ*   \n\n*Title :* " + _0x4123ae.title + "\nUrl : " + _0x4123ae.url + "\n*🌞Description :* " + _0x4123ae.timestamp + "\n*🌞Views :* " + _0x4123ae.views + "\n*🌞Uploaded :* " + _0x4123ae.ago + "\n*😇Author :* " + _0x4123ae.author.name + "\n\n\n_Reply 1 for Video_ Or _1 document_\n_Reply 2 for Audio_ Or _2 document_";
      let _0x3885cc = await smdBuffer(_0x4123ae.thumbnail);
      var _0x44a363 = {
        ...(await _0x2c2023.bot.contextInfo(Config.botname, "ʏᴏᴜᴛᴜʙᴇ ꜱᴏɴɢ", _0x3885cc))
@@ -1654,7 +1654,7 @@ smd({
      }
      let _0x3b2ca6 = await yts(_0x4ec99f);
      let _0x4123ae = _0x3b2ca6.all[0];
-     let _0x5883a9 = "\t *VENOCYBER-ᴍᴅ • sᴏɴɢ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ*   \n\n*Title :* " + _0x4123ae.title + "\nUrl : " + _0x4123ae.url + "\n*Description :* " + _0x4123ae.timestamp + "\n*Views :* " + _0x4123ae.views + "\n*Uploaded :* " + _0x4123ae.ago + "\n*Author :* " + _0x4123ae.author.name + "\n\n\n_Reply 1 for Video_ Or _1 document_\n_Reply 2 for Audio_ Or _2 document_";
+     let _0x5883a9 = "\t *MRSKY-ᴍᴅ • sᴏɴɢ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ*   \n\n*Title :* " + _0x4123ae.title + "\nUrl : " + _0x4123ae.url + "\n*Description :* " + _0x4123ae.timestamp + "\n*Views :* " + _0x4123ae.views + "\n*Uploaded :* " + _0x4123ae.ago + "\n*Author :* " + _0x4123ae.author.name + "\n\n\n_Reply 1 for Video_ Or _1 document_\n_Reply 2 for Audio_ Or _2 document_";
      let _0x3885cc = await smdBuffer(_0x4123ae.thumbnail);
      var _0x44a363 = {
        ...(await _0x2c2023.bot.contextInfo(Config.botname, "ʏᴏᴜᴛᴜʙᴇ ꜱᴏɴɢ", _0x3885cc))
@@ -1681,7 +1681,7 @@ smd({
        return await _0x1c8285.reply("*_Give Me Search Query!_*");
      }
      let _0x2878ec = await yts(_0xca939c);
-     let _0x4186e4 = "*VENOCYBER-ᴍᴅ • ʏᴏᴜᴛᴜʙᴇ ᴅᴏᴡɴʟᴏᴀᴅ* \n*_______________________________* \n\n_Reply Any Number To Download._\n  _For Audio: 1 mp3._\n  _For Video: 1 video._\n  _For document: 1 document._\n\n_Results For : " + _0xca939c + "_ \n\n";
+     let _0x4186e4 = "*MRSKY-ᴍᴅ • ʏᴏᴜᴛᴜʙᴇ ᴅᴏᴡɴʟᴏᴀᴅ* \n*_______________________________* \n\n_Reply Any Number To Download._\n  _For Audio: 1 mp3._\n  _For Video: 1 video._\n  _For document: 1 document._\n\n_Results For : " + _0xca939c + "_ \n\n";
      let _0x463366 = 1;
      for (let _0x308e22 of _0x2878ec.all) {
        _0x4186e4 += " \n*" + _0x463366++ + " : " + _0x308e22.title + (_0x308e22.timestamp ? "(" + _0x308e22.timestamp + ")" : "") + "*\n*Url : " + _0x308e22.url + "*";
@@ -1969,13 +1969,13 @@ smd({
  }) => {
    if (_0xb75e78.quoted && _0xb75e78.text) {
      const _0x5b8ee5 = _0xb75e78.quoted.text.split("\n");
-     if (_0x5b8ee5[0].includes("VENOCYBER-ᴍᴅ • sᴏɴɢ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ")) {
+     if (_0x5b8ee5[0].includes("MRSKY-ᴍᴅ • sᴏɴɢ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ")) {
        const _0x1724ba = _0x5b8ee5.find(_0x525632 => _0x525632.startsWith("Url :"));
        let _0x43a95e = _0x1724ba.replace("Url :", "").trim();
        try {
          await _0xb75e78.sendMessage(_0xb75e78.chat, {
            react: {
-             text: "✨",
+             text: "🐦",
              key: _0xb75e78.key
            }
          });
@@ -2022,7 +2022,7 @@ smd({
        } catch (_0x189dd8) {
          return await _0xb75e78.reply("Error While Downloading Video : " + _0x189dd8);
        }
-     } else if (_0x5b8ee5[0].includes("VENOCYBER-ᴍᴅ • ʏᴏᴜᴛᴜʙᴇ ᴅᴏᴡɴʟᴏᴀᴅ")) {
+     } else if (_0x5b8ee5[0].includes("MRSKY-ᴍᴅ • ʏᴏᴜᴛᴜʙᴇ ᴅᴏᴡɴʟᴏᴀᴅ")) {
        let _0x307bb6 = "*" + _0xb75e78.text.split(" ")[0] + " : ";
        const _0x56275d = _0x5b8ee5.find(_0x3b5e74 => _0x3b5e74.startsWith(_0x307bb6));
        if (_0x56275d) {
@@ -2033,7 +2033,7 @@ smd({
            if (_0x37a579.startsWith("http")) {
              await _0xb75e78.sendMessage(_0xb75e78.chat, {
                react: {
-                 text: "✨",
+                 text: "🐦",
                  key: _0xb75e78.key
                }
              });
@@ -2062,7 +2062,7 @@ smd({
            _0xb75e78.error(_0x3de0e2 + "\n\nCommand yts Listener", _0x3de0e2, "*Video Not Found!*");
          }
        }
-     } else if (_0x5b8ee5[0].includes("VENOCYBER-ᴍᴅ • ᴀᴘᴋ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪsᴛ")) {
+     } else if (_0x5b8ee5[0].includes("MRSKY-ᴍᴅ • ᴀᴘᴋ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪsᴛ")) {
        let _0x35d668 = "*" + _0xb75e78.text.split(" ")[0] + " : ";
        const _0x205a5f = _0x5b8ee5.find(_0x304058 => _0x304058.startsWith(_0x35d668));
        if (_0x205a5f) {
@@ -2072,15 +2072,15 @@ smd({
            const _0x2407a8 = _0x14618c.split("*")[1].replace("Id : ", "").trim();
            await _0xb75e78.sendMessage(_0xb75e78.chat, {
              react: {
-               text: "✨",
+               text: "🐦",
                key: _0xb75e78.key
              }
            });
            let _0x37b3a4 = await download(_0x2407a8);
-           let _0x478b37 = "*App Name :* " + _0x37b3a4.name;
-           _0x478b37 += "\n*App id        :* " + _0x37b3a4.package;
-           _0x478b37 += "\n*Last Up       :* " + _0x37b3a4.lastup;
-           _0x478b37 += "\n*App Size     :* " + _0x37b3a4.size;
+           let _0x478b37 = "*▶App Name :* " + _0x37b3a4.name;
+           _0x478b37 += "\n*▶App id        :* " + _0x37b3a4.package;
+           _0x478b37 += "\n*▶Last Up       :* " + _0x37b3a4.lastup;
+           _0x478b37 += "\n*▶App Size     :* " + _0x37b3a4.size;
            _0x478b37 += "\n               \n" + Config.caption;
            let _0x5032aa = {
              document: {
@@ -2295,7 +2295,7 @@ smd({
       var _0x426e7e = {
         ...(await _0x5dab40.bot.contextInfo(Config.botname, "ᴠɪᴅᴇᴏ ᴅᴏᴡɴʟᴏᴀᴅᴇʀ"))
       };
-      let _0x452f00 = _0x5c3baf.title || _0x57ada1 || _0x13f167[0x1] || "Venocyber MD -- YT Video";
+      let _0x452f00 = _0x5c3baf.title || _0x57ada1 || _0x13f167[0x1] || "Mrsky MD -- YT Video";
       if (_0x57ada1) {
         await _0x5dab40.bot.sendMessage(_0x5dab40.chat, {
           [_0xb0f63e]: {
