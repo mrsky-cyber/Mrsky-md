3aa885ecbb06dbd8ebf31b5fe76631729b02790d
mrsky <126326692+mrsky-cyber@users.noreply.github.com>
2024-07-01 11:17:55 +0300

    Update _updater.js

--- plugins/_updater.js
+++ plugins/_updater.js
@@ -51,7 +51,7 @@ try {
         let update = await DB.sync();
         await citel.bot.sendMessage(
           citel.chat,
-          { text: update.replace(/MrskyTechIMd/, "Astropeda") },
+          { text: update.replace(/SuhailTechIMd/, "Astropeda") },
           { quoted: citel }
         );
         if (
